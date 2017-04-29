22bc290b4c47c266198d9dce41b8fef0944dc683
Abhishek Srivastava <abhsrivastava@researchnow.com>
2017-04-29 18:07:39 -0500

    removed debug statements

--- src/main/scala/TestPartialMock.scala
+++ src/main/scala/TestPartialMock.scala
@@ -7,8 +7,6 @@ import com.google.inject.Inject
 class TestPartialMock @Inject()(t1: Test1, t2: Test2) {
    def test3() = "I do test3"
    def workHorse() : List[String] = {
-      if (t1 == null) println("t1 is null")
-      if (t2 == null) println("t2 is null")
       List(t1.test1(), t2.test2(), test3())
    }
 }
